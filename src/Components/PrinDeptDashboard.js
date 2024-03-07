017158c85405cc3fb900b0a1ba50b1543c54be2a
Gude Anudeep <anudeepgude765@gmail.com>
2024-03-07 10:17:46 +0000

    PrinDeptDashboard is under Dev

--- src/Components/PrinDeptDashboard.js
+++ src/Components/PrinDeptDashboard.js
@@ -9,11 +9,14 @@ const PrinDeptDashboard = () => {
     <div className='h-screen'>
         <NavbarComp />
         <div className='w-full h-full bg-slate-100 flex flex-col items-center'>
+          <div className='p-4 my-4 w-full text-center text-2xl font-semibold'>
+            Welcome to <span className='text-4xl font-bold'>{dept.toLocaleUpperCase()}</span> Department Dashboard
+          </div>
         <div className='md:w-[85%] w-[99%] md:p-12 p-3 gap-x-4 gap-y-5 flex flex-wrap justify-center sm:justify-start '>
-            <div className='md:w-[250px] md:h-[250px] w-[150px] h-[150px] rounded-md hover:shadow-lg border border-black'></div>
-            <div className='md:w-[250px] md:h-[250px] w-[150px] h-[150px] rounded-md hover:shadow-lg border border-black'></div>
-            <div className='md:w-[250px] md:h-[250px] w-[150px] h-[150px] rounded-md hover:shadow-lg border border-black'></div>
-            <div className='md:w-[250px] md:h-[250px] w-[150px] h-[150px] rounded-md hover:shadow-lg border border-black'></div>
+            <div className='md:w-[200px] md:h-[200px] w-[150px] h-[150px] rounded-md hover:shadow-lg border border-black'></div>
+            <div className='md:w-[200px] md:h-[200px] w-[150px] h-[150px] rounded-md hover:shadow-lg border border-black'></div>
+            <div className='md:w-[200px] md:h-[200px] w-[150px] h-[150px] rounded-md hover:shadow-lg border border-black'></div>
+            <div className='md:w-[200px] md:h-[200px] w-[150px] h-[150px] rounded-md hover:shadow-lg border border-black'></div>
         </div>
 
         </div>         
