be8fb98c7d75ade8837bc7631cf76deeadc615c8
prasannagunam <119108413+prasannagunam@users.noreply.github.com>
2024-03-10 22:19:14 +0530

    tnp

--- src/Components/Tnp.js
+++ src/Components/Tnp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavbarComp from './NavbarComp'
 import { FcDepartment } from "react-icons/fc";
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 
