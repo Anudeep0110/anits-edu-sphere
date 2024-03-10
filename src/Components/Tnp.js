6a9b0aa0ab2fde9d0aa1c0895a25e02a84628c06
prasannagunam <119108413+prasannagunam@users.noreply.github.com>
2024-03-10 22:24:24 +0530

    Dashboard update

--- src/Components/Tnp.js
+++ src/Components/Tnp.js
@@ -23,7 +23,7 @@ const navigate = useNavigate();
             <div className='p-10 md:w-[80%] w-full flex flex-wrap md:gap-12 gap-x-24 gap-y-3'>
             <div className='flex flex-col text-center font-semibold hover:font-bold text-gray-900'>
                         <div onClick={() => {navigate('/dashboard',{state:{role:'tnp',fname:'Traning and Placement'}})}} className='bg-[#1f4886] rounded text-white font-medium md:text-5xl text-3xl shadow hover:scale-105 hover:shadow-black hover:shadow-lg flex justify-center items-center w-[75px] md:w-[100px] md:h-[100px] h-[75px]'><FcDepartment /></div>
-                        <p className='text-black md:text-md text-xs'>Fill the form</p>
+                        <p className='text-black md:text-md text-xs'>Fill forms</p>
                         </div>
                 {forms.map((form,index) => {
                     return (
